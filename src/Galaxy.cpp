7da57ead984e6de604b09af79ecbf463387e3169
Bernd Doser <bernd.doser@braintwister.eu>
2017-07-05 16:34:19 +0200

    intended error to test jenkins

--- src/Galaxy.cpp
+++ src/Galaxy.cpp
@@ -3,7 +3,7 @@
 
 double Galaxy::get_mass() const
 {
-  return std::accumulate(stars.begin(), stars.end(), 0.0,
+  return std::accumulate(stars.begin(), stars.end(), 1.0,
          [](double mass , Star const& star) {
            return mass + star.get_mass();
          });
